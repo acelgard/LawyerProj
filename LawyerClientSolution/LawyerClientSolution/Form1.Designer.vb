db48b241c0c843292a6684d00f0b7414d88b29c2
jpkasbohm <jpkasbohm@students.unwsp.edu>
2018-11-29 13:19:28 -0600

    testing connectivity to database 11/29/18

---
+++ LawyerClientSolution/LawyerClientSolution/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmConnect
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblClientID = New System.Windows.Forms.Label()
+        Me.lblFirst = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(209, 43)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 0
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(209, 69)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Client ID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "First Name:"
+        '
+        'lblClientID
+        '
+        Me.lblClientID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblClientID.Location = New System.Drawing.Point(79, 42)
+        Me.lblClientID.Name = "lblClientID"
+        Me.lblClientID.Size = New System.Drawing.Size(100, 23)
+        Me.lblClientID.TabIndex = 4
+        '
+        'lblFirst
+        '
+        Me.lblFirst.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFirst.Location = New System.Drawing.Point(79, 69)
+        Me.lblFirst.Name = "lblFirst"
+        Me.lblFirst.Size = New System.Drawing.Size(100, 23)
+        Me.lblFirst.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(13, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(158, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Reading database info. practice"
+        '
+        'frmConnect
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(302, 115)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblFirst)
+        Me.Controls.Add(Me.lblClientID)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Name = "frmConnect"
+        Me.Text = "Connectivity Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblClientID As Label
+    Friend WithEvents lblFirst As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ LawyerClientSolution/LawyerClientSolution/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+
+'Justin Kasbohm and Andrew Elgard
+
+Public Class frmConnect
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        Dim connectionString As String = "Data Source =MIS-W10-014\SQLEXPRESS;Initial Catalog=LawyerClientProject;Integrated Security=true"
+
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+            MessageBox.Show("Database is open")
+
+            lblClientID.Text =
+            lblClientID.Text = 
+
+            connection.Close()
+            MessageBox.Show("Database is closed")
+
+        End Using
+    End Sub
+End Class
