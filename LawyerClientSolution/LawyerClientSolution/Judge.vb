49f04a04e8151b197ba3b7feb007a7dc9e2a89c7
jpkasbohm <45467134+jpkasbohm@users.noreply.github.com>
2018-12-10 17:04:11 -0600

    JUDGE ADD WORKS

--- LawyerClientSolution/LawyerClientSolution/Judge.vb
+++ LawyerClientSolution/LawyerClientSolution/Judge.vb
@@ -18,28 +18,20 @@ Public Class frmJudge
         Dim connectionString As String = "Data Source =MIS-W10-014\SQLEXPRESS;Initial Catalog=LawyerClientProject;Integrated Security=true"
 
         Using connection As New SqlConnection(connectionString)
-            'I put "ADD" instead of "SELECT" right here in order to input info
-            'Dim queryString As String = "ADD JudgeID, CourtID, FirstName, LastName FROM dbo.Judge;"
-            Dim command As New SqlCommand("Insert into Judge(JudgeID, CourtID, FirstName, LastName) values('" & txtJudgeID.Text & "','" & txtCourtID.Text & "', '" & txtFirstName.Text & "', " & txtLastName.Text & ")", connection)
-
             connection.Open()
             MessageBox.Show("Database is open")
 
-            If command.ExecuteNonQuery = 1 Then
-                MessageBox.Show("New User Added")
+            Dim Command As New SqlCommand("INSERT INTO Judge (JudgeID, CourtID, FirstName, LastName) VALUES ('" & txtJudgeID.Text & "','" & txtCourtID.Text & "', '" & txtFirstName.Text & "', '" & txtLastName.Text & "')", connection)
 
-            Else
-                MessageBox.Show("User Not Added")
+            Command.ExecuteNonQuery()
 
-            End If
+            MessageBox.Show("Row added")
 
+            connection.Close()
             MessageBox.Show("Database is closed")
 
         End Using
 
     End Sub
 
-    Private Sub frmJudgeInput_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
