16b6980d5d4e77396ca37a34120a2c37a2abf05c
Andrew Elgard <acelgard@students.unwsp.edu>
2018-12-13 14:12:10 -0600

    display -paralegal -lawyer

---
+++ LawyerClientSolution/LawyerClientSolution/LawyerDisplay.Designer.vb
@@ -0,0 +1,352 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLawyerDisplay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblLawyerID = New System.Windows.Forms.Label()
+        Me.lblCity = New System.Windows.Forms.Label()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblZip = New System.Windows.Forms.Label()
+        Me.lblFirmAssoc = New System.Windows.Forms.Label()
+        Me.lblSpecialty = New System.Windows.Forms.Label()
+        Me.lblState = New System.Windows.Forms.Label()
+        Me.lblStreet = New System.Windows.Forms.Label()
+        Me.lblLawDegree = New System.Windows.Forms.Label()
+        Me.lblMiddleInitial = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(27, 94)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(86, 20)
+        Me.Label12.TabIndex = 35
+        Me.Label12.Text = "First Name"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(27, 144)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(96, 20)
+        Me.Label11.TabIndex = 34
+        Me.Label11.Text = "Middle Initial"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(27, 194)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(86, 20)
+        Me.Label10.TabIndex = 33
+        Me.Label10.Text = "Last Name"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(27, 343)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(95, 20)
+        Me.Label9.TabIndex = 32
+        Me.Label9.Text = "Law Degree"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(27, 440)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(126, 20)
+        Me.Label8.TabIndex = 31
+        Me.Label8.Text = "Firm Association"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(27, 246)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(99, 20)
+        Me.Label7.TabIndex = 30
+        Me.Label7.Text = "Date of Birth"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(27, 392)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 20)
+        Me.Label6.TabIndex = 29
+        Me.Label6.Text = "Specialty"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(464, 43)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 20)
+        Me.Label5.TabIndex = 28
+        Me.Label5.Text = "Street"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(464, 94)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 20)
+        Me.Label4.TabIndex = 27
+        Me.Label4.Text = "City"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(464, 144)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 20)
+        Me.Label3.TabIndex = 26
+        Me.Label3.Text = "State"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(464, 194)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 20)
+        Me.Label2.TabIndex = 25
+        Me.Label2.Text = "Zip"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 43)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 20)
+        Me.Label1.TabIndex = 24
+        Me.Label1.Text = "Lawyer ID"
+        '
+        'lblLawyerID
+        '
+        Me.lblLawyerID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLawyerID.Location = New System.Drawing.Point(141, 40)
+        Me.lblLawyerID.Name = "lblLawyerID"
+        Me.lblLawyerID.Size = New System.Drawing.Size(175, 26)
+        Me.lblLawyerID.TabIndex = 48
+        '
+        'lblCity
+        '
+        Me.lblCity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCity.Location = New System.Drawing.Point(545, 88)
+        Me.lblCity.Name = "lblCity"
+        Me.lblCity.Size = New System.Drawing.Size(175, 26)
+        Me.lblCity.TabIndex = 49
+        '
+        'lblDOB
+        '
+        Me.lblDOB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDOB.Location = New System.Drawing.Point(141, 246)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(175, 26)
+        Me.lblDOB.TabIndex = 50
+        '
+        'lblLastName
+        '
+        Me.lblLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLastName.Location = New System.Drawing.Point(141, 194)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(175, 26)
+        Me.lblLastName.TabIndex = 51
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFirstName.Location = New System.Drawing.Point(141, 91)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(175, 26)
+        Me.lblFirstName.TabIndex = 52
+        '
+        'lblZip
+        '
+        Me.lblZip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblZip.Location = New System.Drawing.Point(545, 194)
+        Me.lblZip.Name = "lblZip"
+        Me.lblZip.Size = New System.Drawing.Size(136, 26)
+        Me.lblZip.TabIndex = 53
+        '
+        'lblFirmAssoc
+        '
+        Me.lblFirmAssoc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFirmAssoc.Location = New System.Drawing.Point(161, 434)
+        Me.lblFirmAssoc.Name = "lblFirmAssoc"
+        Me.lblFirmAssoc.Size = New System.Drawing.Size(632, 26)
+        Me.lblFirmAssoc.TabIndex = 54
+        '
+        'lblSpecialty
+        '
+        Me.lblSpecialty.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSpecialty.Location = New System.Drawing.Point(161, 386)
+        Me.lblSpecialty.Name = "lblSpecialty"
+        Me.lblSpecialty.Size = New System.Drawing.Size(632, 26)
+        Me.lblSpecialty.TabIndex = 55
+        '
+        'lblState
+        '
+        Me.lblState.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblState.Location = New System.Drawing.Point(545, 138)
+        Me.lblState.Name = "lblState"
+        Me.lblState.Size = New System.Drawing.Size(76, 26)
+        Me.lblState.TabIndex = 56
+        '
+        'lblStreet
+        '
+        Me.lblStreet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblStreet.Location = New System.Drawing.Point(545, 37)
+        Me.lblStreet.Name = "lblStreet"
+        Me.lblStreet.Size = New System.Drawing.Size(248, 26)
+        Me.lblStreet.TabIndex = 57
+        '
+        'lblLawDegree
+        '
+        Me.lblLawDegree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLawDegree.Location = New System.Drawing.Point(161, 337)
+        Me.lblLawDegree.Name = "lblLawDegree"
+        Me.lblLawDegree.Size = New System.Drawing.Size(632, 26)
+        Me.lblLawDegree.TabIndex = 58
+        '
+        'lblMiddleInitial
+        '
+        Me.lblMiddleInitial.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMiddleInitial.Location = New System.Drawing.Point(141, 142)
+        Me.lblMiddleInitial.Name = "lblMiddleInitial"
+        Me.lblMiddleInitial.Size = New System.Drawing.Size(64, 26)
+        Me.lblMiddleInitial.TabIndex = 59
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(653, 525)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 35)
+        Me.btnExit.TabIndex = 84
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(509, 525)
+        Me.btnDisplay.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(112, 35)
+        Me.btnDisplay.TabIndex = 83
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'frmLawyerDisplay
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(904, 616)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lblMiddleInitial)
+        Me.Controls.Add(Me.lblLawDegree)
+        Me.Controls.Add(Me.lblStreet)
+        Me.Controls.Add(Me.lblState)
+        Me.Controls.Add(Me.lblSpecialty)
+        Me.Controls.Add(Me.lblFirmAssoc)
+        Me.Controls.Add(Me.lblZip)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.lblCity)
+        Me.Controls.Add(Me.lblLawyerID)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmLawyerDisplay"
+        Me.Text = "Lawyer Display"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblLawyerID As Label
+    Friend WithEvents lblCity As Label
+    Friend WithEvents lblDOB As Label
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblZip As Label
+    Friend WithEvents lblFirmAssoc As Label
+    Friend WithEvents lblSpecialty As Label
+    Friend WithEvents lblState As Label
+    Friend WithEvents lblStreet As Label
+    Friend WithEvents lblLawDegree As Label
+    Friend WithEvents lblMiddleInitial As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnDisplay As Button
+End Class
---
+++ LawyerClientSolution/LawyerClientSolution/ParalegalDisplay.Designer.vb
@@ -0,0 +1,305 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmParalegalDisplay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblMiddleInitial = New System.Windows.Forms.Label()
+        Me.lblLawDegree = New System.Windows.Forms.Label()
+        Me.lblSpecialty = New System.Windows.Forms.Label()
+        Me.lblFirmAssoc = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.lblParalegalID = New System.Windows.Forms.Label()
+        Me.lblHours = New System.Windows.Forms.Label()
+        Me.lblLawyerID = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(547, 534)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(112, 35)
+        Me.btnSave.TabIndex = 73
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(685, 534)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 35)
+        Me.btnExit.TabIndex = 72
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(29, 88)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(86, 20)
+        Me.Label12.TabIndex = 61
+        Me.Label12.Text = "First Name"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(29, 139)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(96, 20)
+        Me.Label11.TabIndex = 60
+        Me.Label11.Text = "Middle Initial"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(29, 188)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(86, 20)
+        Me.Label10.TabIndex = 59
+        Me.Label10.Text = "Last Name"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(29, 338)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(95, 20)
+        Me.Label9.TabIndex = 58
+        Me.Label9.Text = "Law Degree"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(29, 434)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(126, 20)
+        Me.Label8.TabIndex = 57
+        Me.Label8.Text = "Firm Association"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(29, 241)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(99, 20)
+        Me.Label7.TabIndex = 56
+        Me.Label7.Text = "Date of Birth"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 387)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 20)
+        Me.Label6.TabIndex = 55
+        Me.Label6.Text = "Specialty"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(397, 38)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 20)
+        Me.Label4.TabIndex = 54
+        Me.Label4.Text = "Lawyer ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(397, 88)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 20)
+        Me.Label3.TabIndex = 53
+        Me.Label3.Text = "Hours"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 38)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 20)
+        Me.Label1.TabIndex = 52
+        Me.Label1.Text = "Paralegal ID"
+        '
+        'lblMiddleInitial
+        '
+        Me.lblMiddleInitial.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMiddleInitial.Location = New System.Drawing.Point(143, 137)
+        Me.lblMiddleInitial.Name = "lblMiddleInitial"
+        Me.lblMiddleInitial.Size = New System.Drawing.Size(64, 26)
+        Me.lblMiddleInitial.TabIndex = 81
+        '
+        'lblLawDegree
+        '
+        Me.lblLawDegree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLawDegree.Location = New System.Drawing.Point(188, 338)
+        Me.lblLawDegree.Name = "lblLawDegree"
+        Me.lblLawDegree.Size = New System.Drawing.Size(632, 26)
+        Me.lblLawDegree.TabIndex = 80
+        '
+        'lblSpecialty
+        '
+        Me.lblSpecialty.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSpecialty.Location = New System.Drawing.Point(188, 387)
+        Me.lblSpecialty.Name = "lblSpecialty"
+        Me.lblSpecialty.Size = New System.Drawing.Size(632, 26)
+        Me.lblSpecialty.TabIndex = 79
+        '
+        'lblFirmAssoc
+        '
+        Me.lblFirmAssoc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFirmAssoc.Location = New System.Drawing.Point(188, 435)
+        Me.lblFirmAssoc.Name = "lblFirmAssoc"
+        Me.lblFirmAssoc.Size = New System.Drawing.Size(632, 26)
+        Me.lblFirmAssoc.TabIndex = 78
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFirstName.Location = New System.Drawing.Point(143, 88)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(175, 26)
+        Me.lblFirstName.TabIndex = 77
+        '
+        'lblLastName
+        '
+        Me.lblLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLastName.Location = New System.Drawing.Point(143, 189)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(175, 26)
+        Me.lblLastName.TabIndex = 76
+        '
+        'lblDOB
+        '
+        Me.lblDOB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDOB.Location = New System.Drawing.Point(143, 237)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(175, 26)
+        Me.lblDOB.TabIndex = 75
+        '
+        'lblParalegalID
+        '
+        Me.lblParalegalID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblParalegalID.Location = New System.Drawing.Point(143, 37)
+        Me.lblParalegalID.Name = "lblParalegalID"
+        Me.lblParalegalID.Size = New System.Drawing.Size(175, 26)
+        Me.lblParalegalID.TabIndex = 74
+        '
+        'lblHours
+        '
+        Me.lblHours.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblHours.Location = New System.Drawing.Point(489, 88)
+        Me.lblHours.Name = "lblHours"
+        Me.lblHours.Size = New System.Drawing.Size(76, 26)
+        Me.lblHours.TabIndex = 82
+        '
+        'lblLawyerID
+        '
+        Me.lblLawyerID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLawyerID.Location = New System.Drawing.Point(489, 37)
+        Me.lblLawyerID.Name = "lblLawyerID"
+        Me.lblLawyerID.Size = New System.Drawing.Size(175, 26)
+        Me.lblLawyerID.TabIndex = 83
+        '
+        'frmParalegalDisplay
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(904, 789)
+        Me.Controls.Add(Me.lblLawyerID)
+        Me.Controls.Add(Me.lblHours)
+        Me.Controls.Add(Me.lblMiddleInitial)
+        Me.Controls.Add(Me.lblLawDegree)
+        Me.Controls.Add(Me.lblSpecialty)
+        Me.Controls.Add(Me.lblFirmAssoc)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.lblParalegalID)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmParalegalDisplay"
+        Me.Text = "Paralegal Display"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblMiddleInitial As Label
+    Friend WithEvents lblLawDegree As Label
+    Friend WithEvents lblSpecialty As Label
+    Friend WithEvents lblFirmAssoc As Label
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents lblDOB As Label
+    Friend WithEvents lblParalegalID As Label
+    Friend WithEvents lblHours As Label
+    Friend WithEvents lblLawyerID As Label
+End Class
