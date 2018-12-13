018c095dc0e43a4eb80708d957942dd55ecede5f
Andrew Elgard <acelgard@students.unwsp.edu>
2018-12-13 15:13:55 -0600

    Paralegal -Delete (Lawyer delete was already done)

---
+++ LawyerClientSolution/LawyerClientSolution/ParalegalDelete.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmParalegalDelete
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtParaLegalID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(289, 163)
+        Me.btnDelete.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(112, 35)
+        Me.btnDelete.TabIndex = 20
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 81)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 20)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "ParaLegal ID:"
+        '
+        'txtParaLegalID
+        '
+        Me.txtParaLegalID.Location = New System.Drawing.Point(136, 75)
+        Me.txtParaLegalID.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtParaLegalID.Name = "txtParaLegalID"
+        Me.txtParaLegalID.Size = New System.Drawing.Size(148, 26)
+        Me.txtParaLegalID.TabIndex = 18
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 28)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(496, 20)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Enter the corresponding Court ID of the data you would like to delete."
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(411, 163)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 35)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmParalegalDelete
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(544, 226)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtParaLegalID)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Name = "frmParalegalDelete"
+        Me.Text = "Delete Paralegal Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtParaLegalID As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnExit As Button
+End Class
